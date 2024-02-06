8fe8e4f72da36c96ae252e2e2de016df4aac78fa
[manvanth] <manvanth.me@gmail.com>
2024-02-06 23:53:33 +0530

    Added Home and About pages.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Header = () => {
   return (
-    <div>Header</div>
+    <Header className="">
+        <nav className="">
+            <div className="">
+            <Link to="/"></Link>
+            </div>
+        </nav>
+    </Header>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom/client';
 import  './index.css'
 import { Route,  RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
+import Home from './components/Home/Home'
+import About from './components/About/About'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
-
+    <Route path='' element = {<Home/>}/>
+    <Route path='about' element={<About/>}/>
     </Route>
   )
 )
