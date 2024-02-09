d4c2c8d21a622efad5df91e6cec7083cf8442478
[manvanth] <manvanth.me@gmail.com>
2024-02-09 23:56:21 +0530

    Coded the Home page and completed setting up the routes for home and about

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
-import './App.css'
+import {BrowserRouter, Route, Routes, Link, NavLink} from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-300'>ReactRouterDom</h1>
+    <BrowserRouter>
+    <header>
+      <nav>
+      <h1>Jobrouter</h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+      </nav>
+      </header>
+      <main>
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='About' element={<About/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
     </>
   )
 }
+
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h2>Users</h2>
+        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos explicabo
+         beatae nisi? Esse dignissimos unde provident aperiam repellat id magnam nihil 
+         rem, ab tempore ad error iure? Ea, quisquam culpa!</p>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+         sequi ad, alias ratione iusto exercitationem quisquam illum eum rerum suscipit
+          provident aperiam, vero, harum est voluptates saepe sed voluptate rem.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora sint porro
+         distinctio veritatis quia harum? Tempora quibusdam eius non eveniet, commodi
+          sit illo eum dicta officia consequuntur. Corporis, dolorem officiis.</p>
+    </div>
+  )
+}
+
+export default Home
